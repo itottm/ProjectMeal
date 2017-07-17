9def4fa5f6827ac808d0cc1dfe66c8da0b6fc1a5
伊藤 <rhcp2106@fork.ocn.ne.jp>
2017-07-17 22:58:59 +0900

    Add devise files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717004902) do
+ActiveRecord::Schema.define(version: 20170717125342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(version: 20170717004902) do
   create_table "org_people", force: :cascade do |t|
     t.integer  "typ_position_id"
     t.integer  "org_company_id"
-    t.string   "first_name",             null: false
-    t.string   "last_name",              null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
     t.string   "stripe_publishable_key"
     t.string   "stripe_secret_key"
     t.string   "stripe_user_id"
@@ -72,6 +72,23 @@ ActiveRecord::Schema.define(version: 20170717004902) do
     t.text     "stripe_account_status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_org_people_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_org_people_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_org_people_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "org_products", force: :cascade do |t|
